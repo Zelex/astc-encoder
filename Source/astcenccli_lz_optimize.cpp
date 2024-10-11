b7f6b37fb3e5d3ed98533f04f28457f4f258ba61
Jon Olick <Jon Olick>
2024-10-11 10:17:46 -0500

    matching quality of oodle texture for same lambda

--- Source/astcenccli_lz_optimize.cpp
+++ Source/astcenccli_lz_optimize.cpp
@@ -1626,8 +1626,8 @@ void optimize_for_lz(uint8_t* data, size_t data_len, int blocks_x, int blocks_y,
     }
 
     // Map lambda from [10, 40] to ...
-    float lambda_10 = 0.5f;
-    float lambda_40 = 0.875f;
+    float lambda_10 = 0.25f;
+    float lambda_40 = 0.5f;
     lambda = lambda_10 + (lambda - 10.0f) * (lambda_40 - lambda_10) / (40.0f - 10.0f);
 
     // Initialize block_size_descriptor once
