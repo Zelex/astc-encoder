84024f97d9c0f62bba43bc26eb1cf466eab2886d
Jon Olick <Jon Olick>
2024-10-14 09:00:45 -0500

    further tweaks to 12x12

--- Source/astcenc_entry.cpp
+++ Source/astcenc_entry.cpp
@@ -120,7 +120,7 @@ static const std::array<astcenc_preset_config, 7> preset_configs_mid {{
 static const std::array<astcenc_preset_config, 7> preset_configs_low {{
 	{
 		ASTCENC_PRE_RDO,
-		4, 256, 256, 256, 40, 4, 8, 8, 8, 8, 200.0f, 200.0f, 10.0f, 2.0f, 2.0f, 0.99f, 1.0f
+		4, 256, 256, 256, 100, 4, 8, 8, 8, 8, 200.0f, 200.0f, 10.0f, 2.0f, 2.0f, 0.99f, 1.0f
 	}, {
 		ASTCENC_PRE_FASTEST,
 		2, 10, 6, 4, 40, 2, 2, 2, 2, 2, 85.0f, 63.0f, 3.5f, 1.0f, 1.0f, 0.80f, 1.0f
