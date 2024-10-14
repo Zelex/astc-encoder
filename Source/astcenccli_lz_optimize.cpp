8ba5b07681232c47910a3c85758a6a0f561b38b4
Jon Olick <Jon Olick>
2024-10-14 08:56:21 -0500

    tweaks for 12x12,8x6,etc

--- Source/astcenccli_lz_optimize.cpp
+++ Source/astcenccli_lz_optimize.cpp
@@ -1326,7 +1326,7 @@ void optimize_for_lz(uint8_t* data, size_t data_len, int blocks_x, int blocks_y,
 
     // Map lambda from [10, 40] to ...
     float lambda_10 = 0.025f;
-    float lambda_40 = 0.175f;
+    float lambda_40 = 0.2f;
     lambda = lambda_10 + (lambda - 10.0f) * (lambda_40 - lambda_10) / (40.0f - 10.0f);
 
     // Initialize block_size_descriptor once
